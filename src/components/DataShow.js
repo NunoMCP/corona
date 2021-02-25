694b3b3cccae94015ced3a45d076ed3e6870c292
NunoMCP <NunoMCP@users.noreply.github.com>
2021-02-25 20:49:38 +0000

    grid refactor

--- src/components/DataShow.js
+++ src/components/DataShow.js
@@ -4,8 +4,8 @@ class dataShow extends React.Component{
     render(){
         return(
             <div className="column">
-                <h2 class="ui center aligned icon header">
-                    <i class="circular users icon"></i>
+                <h2 className="ui center aligned icon header">
+                    <i className="circular users icon"></i>
                     {this.props.text}{this.props.value}<br/>
                     valores anteriores em q tenho de fazer a diferença
                 </h2>
--- src/reducers/dataReducer.js
+++ src/reducers/dataReducer.js
@@ -1,5 +1,6 @@
 const reducerSwitch = (state = {}, action) => {
     if(action.type === "GET_DATA"){
+        console.log(action.payload)
         return {...state, thisData: action.payload.data
         }
     } else {
